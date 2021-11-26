98c6263eb1c2afd64f434a7d6ec8a9f760e6a32e
Parker Manning <parkerlmanning@hotmail.com>
2021-11-26 09:47:37 -0600

    tailwind css

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    ehllo
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  created() {
+      console.log(123)
+  }
+}
+</script>
