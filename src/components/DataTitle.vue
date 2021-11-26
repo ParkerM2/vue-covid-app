f616a3314e9dcb5b54ec03dd9d4a763344a7b956
Parker Manning <parkerlmanning@hotmail.com>
2021-11-26 11:43:38 -0600

    api working displaying data, clear button functional, date fixed using momentjs

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-center">
+    <h2 class="text-3x1 font-bold">{{ text }}</h2>
+    <div class="text-2x1 mt-4 mb-10">
+        {{timestamp}}
+    </div>
+    </div>
+</template>
+
+<script>
+    // import { ref } from 'vue';
+    import dayjs from 'dayjs';
+    import {computed } from 'vue';
+    
+    export default {
+        name: 'DataTitle',
+        props: ['text', 'dataDate'],
+        
+        setup () {
+            return {
+                timestamp: dayjs().format('MMMM D YYYY, h:mm:ss a')
+        }    
+    }
+    }
+</script>
